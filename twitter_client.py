3a9c2617ecde6bf4ad0133ad970776295bb4400f
marzgod <starofmaria@gmail.com>
2026-08-06 20:31:52 -0400

    Update twitter_client.py

--- twitter_client.py
+++ twitter_client.py
@@ -1,9 +1,20 @@
-import tweepy,os
-client=tweepy.Client(
-    consumer_key=os.environ["API_KEY"],
-    consumer_secret=os.environ["API_SECRET"],
-    access_token=os.environ["ACCESS_TOKEN"],
-    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
-)
-def post(text):
-    client.create_tweet(text=text)
+import os
+
+import tweepy
+
+
+def post_tweet(text: str) -> None:
+    if not text:
+        raise ValueError("Tweet text is empty.")
+
+    client = tweepy.Client(
+        consumer_key=os.environ["API_KEY"],
+        consumer_secret=os.environ["API_SECRET"],
+        access_token=os.environ["ACCESS_TOKEN"],
+        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
+    )
+
+    response = client.create_tweet(text=text)
+
+    if not response.data:
+        raise RuntimeError("X did not return tweet data.")
